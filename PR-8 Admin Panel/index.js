14c2270e705ab24d44d09e6675e57430965c4460
hevin08 <matariyahevin@gmail.com>
2025-01-29 07:46:25 +0530

    finel

--- PR-8 Admin Panel/index.js
+++ PR-8 Admin Panel/index.js
@@ -32,7 +32,7 @@ app.use(session({
 }))
 app.use(passport.session());
 app.use(passport.initialize());
-app.use(passport.setUser);
+app.use(passport.setUser());
 app.use(express.urlencoded());
 
 app.use(flash());
